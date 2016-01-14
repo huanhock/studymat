0021a4a532904b9e7aab9e0e07ceb0a73f82141f
Eric Tai <ericloong90@gmail.com>
2016-01-14 12:59:11 +0800

    independent-practice.js added

--- 00-programming/js-functions-and-scope/starter-code/independent-practice.js
+++ 00-programming/js-functions-and-scope/starter-code/independent-practice.js
@@ -4,6 +4,20 @@
 
 // your code here
 
+var words = ["Eric", "Tai", "Chi", "Loong"];
+
+function lengths (array) {
+  var answer = array.map(function(elem) {
+    return elem.length;
+  });
+  return answer;
+}
+
+word_length = lengths(words);
+
+console.log(word_length);
+
+
 // 2. Write a Javascript function called `transmogrifier`
 // This function should accept three arguments, which you can assume will be numbers.
 // Your function should return the "transmogrified" result
@@ -13,8 +27,25 @@
 
 // your code here
 
+
+function transmogrifier (a, b, c) {
+  var product = Math.pow(a * b, c);
+  return product;
+}
+
+console.log(transmogrifier(3, 4, 2));
+
+
 // 3. Write a function `wordReverse` that accepts a single argument, a string. The
 // method should return a string with the order of the words reversed. Don't worry
 // about punctuation.
 
 // your code here
+
+var string1 = "D4rknessR3igns";
+
+function wordReverse (element) {
+  return element.split("").reverse().join("");
+}
+
+console.log(wordReverse(string1));
